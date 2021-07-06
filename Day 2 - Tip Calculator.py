65080b2465e0c7b3c267000903fcb50c2a1c6f24
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2021-07-06 17:01:26 +0530

    wrote some code

---
+++ Day 2 - Tip Calculator.py
@@ -0,0 +1,4 @@
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+ppl = int(input("How many people to split the bill? "))
