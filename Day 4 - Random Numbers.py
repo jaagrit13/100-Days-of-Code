259adca68a16deb29565fc076b078a24c4e58e8a
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2021-07-14 22:30:45 +0530

    practice

--- Day 4 - Random Numbers.py
+++ Day 4 - Random Numbers.py
@@ -2,3 +2,9 @@ import random
 
 random_int = random.randint(1,10)
 print(random_int)
+
+random_float = random.random()
+print(random_float)
+
+random_float *= 5 #this will increase its range from 0-1(not including 1) to 0-5 (not including 5)
+print(random_float)
