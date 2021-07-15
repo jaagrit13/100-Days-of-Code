223643eddc44d889691da63ae5d6828dcea0efc7
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2021-07-15 15:14:45 +0530

    Completed

--- Day 4 - Rock Paper and Scissors.py
+++ Day 4 - Rock Paper and Scissors.py
@@ -26,4 +26,31 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-print("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+
+game_images = [rock, paper, scissors]
+
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+
+print(game_images[choice])
+
+choice2 = random.randint(0,2)
+
+print("Computer Chose:\n")
+print(game_images[choice2])
+
+if choice >= 3 or choice < 0:
+    print("You typed an invalid number, you lose!")
+elif choice == choice2:
+    print("Its's a draw")
+elif choice == 0 and choice2 == 2:
+    print("You win!")
+elif choice == 0 and choice2 == 1:
+    print("You lose!")
+elif choice == 1 and choice2 == 2:
+    print("You lose!")
+elif choice == 1 and choice2 == 0:
+    print("You win!")
+elif choice == 2 and choice2 == 1:
+    print("You win!")
+elif choice == 2 and choice2 == 0:
+    print("You lose!")
