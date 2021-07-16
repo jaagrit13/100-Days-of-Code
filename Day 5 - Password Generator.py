10f97a8e562f07c2484e8b53dd487f3142e3dc1e
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2021-07-16 19:05:12 +0530

    Completed

--- Day 5 - Password Generator.py
+++ Day 5 - Password Generator.py
@@ -7,3 +7,17 @@ print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = str()
+
+for i in range(nr_letters):
+    index = random.randint(0,len(letters))
+    password = password + letters[index]
+# print(password)
+for j in range(nr_symbols):
+    index = random.randint(0,len(symbols))
+    password = password + symbols[index]
+for j in range(nr_numbers):
+    index = random.randint(0,len(numbers))
+    password = password + numbers[index]
+print("Here is your password:",password)
