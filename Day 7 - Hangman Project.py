6411138ada9d1eb6f2798fb3c64e29412124a67d
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2021-07-24 22:03:04 +0530

    Completed

--- Day 7 - Hangman Project.py
+++ Day 7 - Hangman Project.py
@@ -1,14 +1,15 @@
 import random
 import hangmanart as ha
-word_list = ["ardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+import hangmanword as hw
+chosen_word = random.choice(hw.word_list)
 word_length = len(chosen_word)
-#Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+# Testing code
+# print(f'Pssst, the solution is {chosen_word}.')
 
 lives = 6
 display = []
 endgame = False
+print(ha.logo)
 
 for i in range(len(chosen_word)):
     display.append("_")
@@ -16,13 +17,17 @@ for i in range(len(chosen_word)):
 while not endgame and  lives > 0:
     guess = input("Guess a letter: ").lower()
 
+    if guess in display:
+      print("You've already guessed {}".format(guess))
+
     for i in range(len(chosen_word)):
         if guess == chosen_word[i]:
             display[i] = guess
     if guess not in chosen_word:
+        print("You guessed {}, that's not in the word. You lose a life.".format(guess))
         lives -= 1
     print(" ".join(display))
-    print(stages[lives])
+    print(ha.stages[lives])
 
     if "_" not in display:
         endgame = True
