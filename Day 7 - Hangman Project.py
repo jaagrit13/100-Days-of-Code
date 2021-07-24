3d545dbbd63d1bd84fd4f3ab2746047d2f85bbd6
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2021-07-24 21:28:57 +0530

    Some Changes

--- Day 7 - Hangman Project.py
+++ Day 7 - Hangman Project.py
@@ -59,4 +59,28 @@ stages = ['''
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
 
+lives = 6
+display = []
+endgame = False
+
+for i in range(len(chosen_word)):
+    display.append("_")
+
+while not endgame and  lives > 0:
+    guess = input("Guess a letter: ").lower()
+
+    for i in range(len(chosen_word)):
+        if guess == chosen_word[i]:
+            display[i] = guess
+    if guess not in chosen_word:
+        lives -= 1
+    print(stages[lives])
+
+    if "_" not in chosen_word:
+        endgame = True
+        print("You win.")
+if lives == 0:
+    print("You lose.")
