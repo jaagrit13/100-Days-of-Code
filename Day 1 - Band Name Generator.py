a992927cb1ba22691fe5809e5386fa077e748c93
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2022-02-27 16:56:55 +0530

    Not Required

--- Day 1 - Band Name Generator.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the Band Name Generator.")
-print("What's name of the city you grew up in?")
-a = input()
-print("What's your pet's name?")
-b = input()
-print("Your Band Name could be " + a + " " + b)
