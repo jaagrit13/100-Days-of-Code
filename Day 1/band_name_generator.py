f7d96e8d7b611abb9ac9035971308aa599fe38ce
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2022-02-27 16:53:07 +0530

    Day - 1 Project

---
+++ Day 1/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew in?\n").capitalize()
+pet_name = input("What's your pet's name?\n").capitalize()
+print("Your band name could be " + city + " " + pet_name)
