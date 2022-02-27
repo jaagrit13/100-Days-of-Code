bedad8d969ab4c6d40f7857a7424c9c59c87d53f
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2022-02-27 16:19:42 +0530

    Replit Solution

---
+++ Day 1/Exercise_1.2.py
@@ -0,0 +1,4 @@
+print("Day 1 - String Manipulation") #Starting double quote was missing
+print('String Concatenation is done with the "+" sign.')#Single Quotes insted of Double Quotes
+print('e.g. print("Hello " + "world")')#Indentation Error
+print("New lines can be created with a backslash and n.")#Extra starting bracket
