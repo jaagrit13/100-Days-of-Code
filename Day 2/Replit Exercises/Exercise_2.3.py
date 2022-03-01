6e533943c8751d76ee809cc24b2949aed17c8c18
Jaagrit <jaagrit0306.cse19@chitkara.edu.in>
2022-03-01 19:15:06 +0530

    Replit Solution

---
+++ Day 2/Replit Exercises/Exercise_2.3.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+age = int(age)
+time_left = 90 - age
+days = time_left*365
+weeks = time_left*52
+months = time_left*12
+
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
